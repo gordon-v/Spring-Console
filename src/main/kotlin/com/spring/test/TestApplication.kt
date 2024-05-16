9bdf23728e54ad955b3b725ec6f5979524d23488
Gordon <gordon.velichkovski@proton.me>
2024-05-16 17:58:55 +0200

    Implemented MatcherUrl class that loads values from env-var and combines into url for matcher

--- src/main/kotlin/com/spring/test/TestApplication.kt
+++ src/main/kotlin/com/spring/test/TestApplication.kt
@@ -2,23 +2,35 @@ package com.spring.test
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
 
-class MatchRequest {
-	val Name: String = "Report Name"
-	val ReportId: Int = 1
+class MatchRequest(val ReportId: Int)
+
+@Component
+class MatcherUrl(
+	@Value("#{environment.SCHEME}") private val scheme: String,
+	@Value("#{environment.HOST}") private val host: String,
+	@Value("#{environment.PORT}") private val port: String
+){
+	private val url: String = "$scheme://$host:$port"
+	fun printUrl(){
+		print(url)
+	}
 }
 
 @SpringBootApplication
-class TestApplication: CommandLineRunner {
+class TestApplication(val matcherUrl: MatcherUrl): CommandLineRunner {
 
 	val LOG: Logger = LoggerFactory.getLogger(TestApplication::class.java)
 
-
 	override fun run(vararg args: String?) {
-
+		matcherUrl.printUrl()
 	}
 }
 
